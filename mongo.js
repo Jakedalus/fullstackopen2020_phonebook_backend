da011b5b150924e671e4aeb4a3c011789441be11
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-09-17 16:27:43 -0400

    Add functionality to allow getting all entries in phonebook or adding new person depending on the legth of the argv list

--- mongo.js
+++ mongo.js
@@ -23,13 +23,30 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema);
 
+if (process.argv.length === 3) {
+	Person.find({}).then(results => {
+		results.forEach(person => console.log(person));
+		mongoose.connection.close();
+	});
+}
+
+if (process.argv.length === 5) {
+	const name = process.argv[3];
+	const number = process.argv[4];
+	const person = new Person({ name, number });
+	person.save().then(result => {
+		console.log('person saved!', result);
+		mongoose.connection.close();
+	});
+}
+
 // create a test person:
 // const person = new Person({
 // 	name   : 'Francis',
 // 	number : '555-555-5555'
 // });
 
-person.save().then(result => {
-	console.log('person saved!', result);
-	mongoose.connection.close();
-});
+// person.save().then(result => {
+// 	console.log('person saved!', result);
+// 	mongoose.connection.close();
+// });
